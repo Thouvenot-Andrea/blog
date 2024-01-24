f97949b333cc7882db5afd33377dbedf754abae2
thouvenot <thouvenot@thouvenot-desktop>
2024-01-24 16:21:29 +0100

    itération 2 2.2/1.

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,2 +1,9 @@
 <?php
+/* Sera le contrôleur de la page d’accueil de votre blog, Il est le chef d’orchestre des actions à réaliser pour
+afficher la page d’accueil. Il va appeler la fonction qui gère les données, faire les vérifications et pour
+finir appeler le rendu.*/
 echo "Hello World";
+
+include ('../blog/app/controllers/homeController.php');
+
+lastBlogPosts($pdo);
---
+++ app/persistances/blogPostData.php
@@ -0,0 +1,9 @@
+<?php
+/* va contenir toutes les fonctions qui seront responsables de gérer la persistance des articles dans la BDD et donc
+du dialogue avec la BDD.*/
+
+function lastBlogPosts($pdo)
+{
+    return [];
+}
+
