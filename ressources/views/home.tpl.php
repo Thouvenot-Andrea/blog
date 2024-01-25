a8f7cc18a57fc58897959e47d595b6bfec0e0ec2
thouvenot <thouvenot@thouvenot-desktop>
2024-01-25 16:43:58 +0100

    home.tpl.php

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -1,22 +1,32 @@
-<?php
-if (isset($post)){
-    echo ("Aucun Article");
-}
-else{
-foreach ($resultPosts  as $post ){
-
-    echo '<br>';
-    echo "Titre : ".
-        $post ['title'] ." //  ";
-    echo "Auteur : ".
-        $post ['Authors_id']." //  ";
-    echo "Texte : ".
-        $post ['text']." //  ";}
-}
 
 
+<?php if (empty($resultPosts)) : ?>
+    <p>Aucun Article trouvé</p>
+<?php else : ?>
+<section>
+    <h1>Titre de l'article</h1>
+    <?php foreach ($resultPosts as $post) : ?>
+        <?= $post ['title'] ?>
+    <?php endforeach; ?>
+<?php endif; ?>
+</section>
 
 
+<!---->
+<!--if (isset($post)){-->
+<!--    echo ("Aucun Article");-->
+<!--}-->
+<!--else{-->
+<!--foreach ($resultPosts  as $post ){-->
+<!---->
+<!--    echo '<br>';-->
+<!--    echo "Titre : ".-->
+<!--        $post ['title'] ." //  ";-->
+<!--    echo "Auteur : ".-->
+<!--        $post ['Authors_id']." //  ";-->
+<!--    echo "Texte : ".-->
+<!--        $post ['text']." //  ";}-->
+<!--}-->
 
-include ('../ressources/views/layouts/header.tpl.php');
-include ('../ressources/views/layouts/footer.tpl.php');
+<!--include ('../ressources/views/layouts/header.tpl.php');-->
+<!--include ('../ressources/views/layouts/footer.tpl.php');-->
